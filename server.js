d33af76d4a743019e933436d23d6eb8e29880c6f
Craig Christensen <ruffcorn@gmail.com>
2018-04-04 16:07:42 -0500

    routes working

--- server.js
+++ server.js
@@ -1,60 +1,63 @@
-// Dependencies
-var express = require("express");
-var exphbs  = require('express-handlebars');
-var request = require("request");
-var cheerio = require("cheerio");
-var mongoose = require('mongoose');
-var bodyParser = require('body-parser');
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// DEPENDENCIES
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+const express = require("express");
+// const exphbs  = require('express-handlebars');
+const bodyParser = require('body-parser');
+const logger = require("morgan");
+const mongoose = require('mongoose');
 
-// Initialize Express
-var app = express();
-var PORT = process.env.PORT || 3000;
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// EXPRESS SERVER
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Initialize Express and handle Heroku deployment
+let app = express();
+const PORT = process.env.PORT || 3000;
+// Use express.static to serve the public folder as a static directory
+app.use(express.static("public"));
 
-// set up handlebars
-app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
-app.set('view engine', '.hbs');
- 
-// app.get('/', function (req, res) {
-//     res.render('home');
-// });
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// MORGAN
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Use morgan logger for logging requests
+app.use(logger("dev"));
 
-// mongoose
-// mongoose.connect('mongodb://localhost/my_database');
-
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// BODY-PARSER MIDDLEWARE
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // body-parser middleware
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
- 
+// Use body-parser for handling form submissions
+app.use(bodyParser.urlencoded({ extended: true }));
 // parse application/json
-app.use(bodyParser.json())
- 
-// app.use(function (req, res) {
-//   res.setHeader('Content-Type', 'text/plain')
-//   res.write('you posted:\n')
-//   res.end(JSON.stringify(req.body, null, 2))
-// })
+app.use(bodyParser.json());
 
-// cheerio
-// const $ = cheerio.load('<h2 class="title">Hello world</h2>')
- 
-// $('h2.title').text('Hello there!')
-// $('h2').addClass('welcome')
- 
-// $.html()
-//=> <html><head></head><body><h2 class="title welcome">Hello there!</h2></body></html> 
 
-// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-
-// Set mongoose to leverage built in JavaScript ES6 Promises
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// MONGOOSE, ORM FOR MONGODB
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI, {
-  useMongoClient: true
+mongoose.connect("mongodb://localhost/hackernews", 
+{
+  // useMongoClient: true
 });
-
-
+// If deployed, use the deployed database. Otherwise use the local mongo
+// Headlines database
 // note: the heroku app is https://git.heroku.com/sheltered-springs-65221.git
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/hackernews";
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// HANDLEBARS
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// set up handlebars to be the default templating engine
+// and change the extension from .handlebars to .hbs
+// app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
+// app.set('view engine', '.hbs');
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Routes
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+require("./routes/api/api-routes.js")(app);
 
 // Listen on port 3000
 app.listen(PORT, function() {
